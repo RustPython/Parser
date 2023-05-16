792a5ae339d5b9e59c51beec321ac8accd5fdc95
Jeong YunWon <jeong@youknowone.org>
2023-05-16 22:40:30 +0900

    is_* methods to ConversionFlag

--- core/src/format.rs
+++ core/src/format.rs
@@ -1,5 +1,5 @@
 /// Transforms a value prior to formatting it.
-#[derive(Copy, Clone, Debug, PartialEq, Eq)]
+#[derive(Copy, Clone, Debug, PartialEq, Eq, is_macro::Is)]
 #[repr(i8)]
 pub enum ConversionFlag {
     /// No conversion
